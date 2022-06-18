5148e4e7a683e11e2647759402da408000543503
Edmundo Guerrero <edmundo.gt.esc@gmail.com>
2022-06-18 13:52:37 -0600

    base final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-// import { ButtonCustom } from "./components/button/ButtonCustom";
-// import { TextCustom } from "./components/text/TextCustom";
-import { Welcome } from "./screens/Welcome";
+import { ProfileCustom } from "./components/profile/ProfileCustom";
+// import { Welcome } from "./screens/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      {
+        // <Welcome />
+      }
+      <ProfileCustom />
     </div>
   );
 }
---
+++ src/components/profile/ProfileCustom.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { TextCustom } from '../text/TextCustom'
+import { ImageCustom } from '../image/ImageCustom'
+import { ButtonCustom } from '../button/ButtonCustom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+
+const ProfileCustom = props => {
+    return (
+        <div className='ProfileCustom'>
+            <div className="ProfileCustom-Name">
+                <TextCustom>
+                    Edmundo Guerrero
+                </TextCustom>
+            </div>
+            <div className="ProfileCustom-Image">
+                <ImageCustom source="./assets/img/avi/avatar.png" />
+            </div>
+            <div className="ProfileCustom-Logout">
+                <ButtonCustom>
+                    {/* <FontAwesomeIcon icon={solid('user-secret')} /> */}
+
+                    {/* <FontAwesomeIcon icon="fa-solid fa-check-square" /> */}
+                </ButtonCustom>
+            </div>
+        </div>
+    )
+}
+
+// ProfileCustom.propTypes = {
+
+// }
+
+export { ProfileCustom }
